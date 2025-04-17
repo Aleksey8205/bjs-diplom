6be471c0760dbe2a7ef9fde32b80dd6c83b53227
aleksey8205 <aleksey8205@yandex.ru>
2025-04-17 15:27:11 +0300

    update home.page

--- public/homePage.js
+++ public/homePage.js
@@ -10,22 +10,57 @@ logOutButton.action = () => {
 
 const profileWidget = new ProfileWidget();
 ApiConnector.current((response) => {
-    if (response.data) {
-        ProfileWidget.showProfile(response.data); 
-    }
+  if (response.data) {
+    ProfileWidget.showProfile(response.data);
+  }
 });
 
 const ratesBoard = new RatesBoard();
 function updateRates() {
-    ApiConnector.getStocks((response) => {
-        if(response.success) {
-            ratesBoard.clearTable();
-            ratesBoard.fillTable(response.data);
-        }
-    });
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
 }
 
 updateRates()
 setInterval(updateRates, 60000)
 
 const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      profileWidget.showProfile(response.data.user);
+      moneyManager.setMessage("Баланс успешно пополнен");
+    } else {
+      moneyManager.setMessage(response.error)
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      profileWidget.showProfile(response.data.user);
+      moneyManager.setMessage("Валюта успешно конвертирована!")
+    } else {
+      moneyManager.setMessage(response.error);
+    }
+  })
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+      if (response.success) {
+          profileWidget.showProfile(response.data.user);
+          moneyManager.setMessage('Валюта успешно конвертирована!');
+      } else {
+          moneyManager.setMessage(response.error);
+      }
+  });
+};
+
+const faworitesWidget = new FavoritesWidget();
